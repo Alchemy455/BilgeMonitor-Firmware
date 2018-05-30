7085a3911f85ecc58e90b2e15177c7f9d4be2d8a
Matthew Barker <31450170+Alchemy455@users.noreply.github.com>
2018-05-30 09:12:03 -0400

    initial commit

---
+++ AlchemyBilgeMonitor/AlchemyBilgeMonitor/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/24/2018 5:31:24 PM
+ * Author : mbarker
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
